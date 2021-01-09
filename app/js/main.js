5df65fa7c11121ffe28cf2e9e34d1290ee0cd7fd
licler <ute3@yandex.ru>
2021-01-09 22:01:40 +0300

    adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -17,16 +17,55 @@ $(function () {
             arrows: true,
             prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/about/arrow-prewious.png" alt=""></button>',
             nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/about/arrow-next.png" alt=""></button>',
-            autoplay: true
-    });
+            autoplay: false,
+            responsive: [
+                {
+                  breakpoint: 1100,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: false
+                  }
+                },
+                {
+                    breakpoint: 750,
+                    settings: {
+                      slidesToShow: 1,
+                      slidesToScroll: 1,
+                      infinite: true,
+                      autoplay: false,
+                      arrows: false
+                    }
+                  }
+            ]
+        });
+
     $('.clients-page__slider').slick({
         infinite: true,
         arrows: false,
         dots: true,
-        autoplay: true
+        autoplay: false,
+        responsive: [
+            {
+              breakpoint: 650,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false
+              }
+            }
+        ]
      });
 
 
+
+ $('.menu__big-btn').on('click', function() {
+    $('.menu-list').slideToggle();
+ });
+ $('.header__top-btn').on('click', function() {
+    $('.header__top-info').toggleClass('active');
+ });
 
 
 
